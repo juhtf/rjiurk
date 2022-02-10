572f92ba8f73041cd8fcf60151c0b0bb8eb69564
juhtf <86907413+juhtf@users.noreply.github.com>
2022-02-10 15:55:59 +0800

    Add files via upload

---
+++ book.js
@@ -0,0 +1,20 @@
+img=" ";
+
+
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+}
+
+function preload(){
+    img=loadImage('book.jfif');
+}
+
+function draw(){
+    image(img,0,0,640,420);
+    fill("#FF0000");
+    text("book",45,75);
+    noFill();
+    stroke("green");
+    rect(30,60,450,350);
+}
